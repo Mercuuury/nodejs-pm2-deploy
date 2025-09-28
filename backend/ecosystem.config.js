1c7038b3f3e78f107ba127256ea4ee5fe7fe9d70
Alexander <84013181+Mercuuury@users.noreply.github.com>
2025-09-28 15:24:49 +0300

    feat: :sparkles: add deployment support via pm2 (#1)
    * feat: :sparkles: add `/crash-test` endpoint
    
    * feat: :construction_worker: add `ecosystem.config.js` & `.env.example` for backend
    
    * feat: :construction_worker: add `ecosystem.config.js` & `.env.deploy.example` for frontend, rename `.env*` to `.env.deploy*`
    
    * feat: :construction_worker: add frontend deploy script, rename `DATABASE_URL` to `DB_ADDRESS`
    
    * feat: :lock: configure cors
    
    * chore: :memo: update `README.md`

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,42 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF,
+  DEPLOY_SSH_KEY_PATH = '~/.ssh/practicum/private_key',
+  PORT = 3001,
+  NODE_ENV = 'production',
+  JWT_SECRET,
+  DB_ADDRESS,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-backend',
+      script: './dist/app.js',
+      env_production: {
+        PORT,
+        NODE_ENV,
+        JWT_SECRET,
+        DB_ADDRESS,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: DEPLOY_SSH_KEY_PATH,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp .env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build && pm2 restart ecosystem.config.js',
+    },
+  },
+};
---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF,
+  DEPLOY_SSH_KEY_PATH = '~/.ssh/practicum/private_key',
+  PORT = 3000,
+  NODE_ENV = 'production',
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'mesto-frontend',
+      script: 'npm',
+      args: 'start',
+      env_production: {
+        PORT,
+        NODE_ENV,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: DEPLOY_SSH_KEY_PATH,
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp .env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build && pm2 restart ecosystem.config.js',
+    },
+  },
+};
